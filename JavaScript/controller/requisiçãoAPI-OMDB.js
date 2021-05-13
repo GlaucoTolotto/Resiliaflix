ddfa82931f24dae774d2f59c74dd9892cac4261d
GlaucoTolotto <glaucotolotto@gmail.com>
2021-05-13 16:12:58 -0300

    Tentativa de condigo para redirecionamento da pagina de informaçoes dos filmes

--- JavaScript/controller/requisiçãoAPI-OMDB.js
+++ JavaScript/controller/requisiçãoAPI-OMDB.js
@@ -1,5 +1,5 @@
 $.ajax({url: http://www.omdbapi.com/?i=tt3896198&apikey=35ec292f,
-success: function(result){
+    success: function(result){
     
     }
 })
--- JavaScript/views/visual.js
+++ JavaScript/views/visual.js
@@ -1,9 +1,40 @@
+
 function openPage() {
     var x = document.getElementById("search").value;
+    let listaDeFilmes = [
+        'De volta para o futuro',
+        'Star Wars',
+        'Star Trek',
+        'Matrix',
+        'Interstellar',
+        '2001 uma odisseia no espaço',
+        'Blade Runner',
+        'A chegada',
+        'Her',
+        'Wall-E',
+        'Jogador Numero 1',
+        'Inception',];
 
-    if (x === "matrix") {
-        window.open("");
+    for (let i = 0; i < listaDeFilmes.length; i++) {
+        console.log(listaDeFilmes)
+        if (x === listaDeFilmes[i].length) {
+            window.open('/deucerto.html')
+        }
+
     }
+    try {
+        for (let i = 0; i < listaDeFilmes.length; i++) {
+            if (x != listaDeFilmes[i].length) {
+                window.open("/erro404.html");
 
+            }
+        }
 
+    } catch (erro) {
+        window.open("/erro404.html");
+
+    }
 }
+
+
+
