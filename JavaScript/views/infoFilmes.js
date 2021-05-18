0521ee9a83aa25a5aba944d4c9d7c62ce08c8b09
GlaucoTolotto <glaucotolotto@gmail.com>
2021-05-18 17:32:08 -0300

    Style das informações dos filmes feita

--- JavaScript/views/infoFilmes.js
+++ JavaScript/views/infoFilmes.js
@@ -2,4 +2,7 @@ function pegaFilme(result) {
     $('#title').html(result.Title);
     $('#plot-do-filme').html(result.Plot);
     $('#image').attr('src', result.Poster);
+    $('#genre').html(result.Genre);
+    $('#director').html(result.Director);
+    $('#critica').html(result.Source);
 }
