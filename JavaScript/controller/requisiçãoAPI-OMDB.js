06450f633a4a143685573be956e3d16e268b092a
GlaucoTolotto <glaucotolotto@gmail.com>
2021-05-18 14:52:44 -0300

    Recuperção de arquivos perdidos por de atualização

--- JavaScript/controller/requisiçãoAPI-OMDB.js
+++ JavaScript/controller/requisiçãoAPI-OMDB.js
@@ -1,11 +1,11 @@
  
-function procuraFilmes (movie, numero){
+function buscaFilme(){
     $.ajax({
-        url: ("http://www.omdbapi.com/?i=tt3896198&apikey=35ec292f"),
+        url: (`http://www.omdbapi.com/?i=${procuraFilmes()}&apikey=35ec292f`),
         success: function(result){
             pegaFilme(result);
         }
     })
-}
+} 
 
 // url: ("http://www.omdbapi.com/?i=" + movie + "tt3896198&apikey=35ec292f"),
